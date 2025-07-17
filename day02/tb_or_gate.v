aead96aa9d0e2b326221db12912ee27d68bc6808
verilog-hk <hariofficial016@gmail.com>
2025-07-17 12:41:41 +0530

    Update tb_or_gate.v

--- day02/tb_or_gate.v
+++ day02/tb_or_gate.v
@@ -1,1 +1,19 @@
+module tb_or_gate;
+  reg a, b;
+  wire c;
+
+  or_gate uut (.a(a), .b(b), .c(c));
+
+  initial begin
+    $display("A B | C");
+    $monitor("%b %b | %b", a, b, c);
+    
+    a = 0; b = 0; #10;
+    a = 0; b = 1; #10;
+    a = 1; b = 0; #10;
+    a = 1; b = 1; #10;
+
+    $finish;
+  end
+endmodule
 
